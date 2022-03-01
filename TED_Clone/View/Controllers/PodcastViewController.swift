ed003439351a467d1ca8a95c2a001a4c2bc827f9
JeongDeokHo <ekdj98@gmail.com>
2022-03-02 00:02:39 +0900

    color extension

--- TED_Clone/View/Controllers/PodcastViewController.swift
+++ TED_Clone/View/Controllers/PodcastViewController.swift
@@ -12,7 +12,7 @@ class PodcastViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemOrange
+        view.backgroundColor = UIColor.backgroundColor
     }
     
 
