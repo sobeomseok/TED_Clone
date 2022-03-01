0ccd101a3f4c1ed571ea6f77c2925fa6cafe8531
MinZombie <78908490+MinZombie@users.noreply.github.com>
2022-03-01 22:18:38 +0900

    프로젝트 세팅2

---
+++ TED_Clone/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  TED_Clone
+//
+//  Created by 민선기 on 2022/03/01.
+//
+
+import Foundation
---
+++ TED_Clone/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  TED_Clone
+//
+//  Created by 민선기 on 2022/03/01.
+//
+
+import Foundation
